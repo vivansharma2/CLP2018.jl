0db328c730f0f4565593c83256fa9a41589421d0
Documenter.jl <documenter@juliadocs.github.io>
2025-01-03 10:24:21 +0000

    build based on 0c07aee

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = New_package","category":"page"},{"location":"#New_package","page":"Home","title":"New_package","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for New_package.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [New_package]","category":"page"}]
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CLP2018","category":"page"},{"location":"#CLP2018","page":"Home","title":"CLP2018","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CLP2018.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CLP2018]","category":"page"}]
 }
